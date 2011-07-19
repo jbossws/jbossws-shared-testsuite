3c830459def59c14765e76780ea1a21d9dde4aec
alessio.soldano@jboss.com <alessio.soldano@jboss.com@50344849-fb0f-0410-a53f-a94597aac05c>
2011-07-19 14:38:40 +0000

    Adding @Resource use in testcase

--- shared-testsuite/branches/asoldano/testsuite/src/test/java/org/jboss/test/ws/publish/EndpointImpl.java
+++ shared-testsuite/branches/asoldano/testsuite/src/test/java/org/jboss/test/ws/publish/EndpointImpl.java
@@ -21,19 +21,34 @@
  */
 package org.jboss.test.ws.publish;
 
+import javax.annotation.Resource;
 import javax.jws.WebService;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
 
 import org.jboss.logging.Logger;
 
 @WebService(serviceName="EndpointService", portName="EndpointPort", endpointInterface = "org.jboss.test.ws.publish.Endpoint")
 public class EndpointImpl
 {
+   @Resource
+   WebServiceContext wsCtx;
+   
    // Provide logging
    private static Logger log = Logger.getLogger(EndpointImpl.class);
 
    public String echo(String input)
    {
       log.info("echo: " + input);
+      MessageContext msgContext = (MessageContext)wsCtx.getMessageContext();
+      if (msgContext == null) {
+         return "MessageContext is null!";
+      }
+      log.info("WSDL_DESCRIPTION: " + msgContext.get(MessageContext.WSDL_DESCRIPTION));
+      log.info("WSDL_SERVICE: " + msgContext.get(MessageContext.WSDL_SERVICE));
+      log.info("WSDL_INTERFACE: " + msgContext.get(MessageContext.WSDL_INTERFACE));
+      log.info("WSDL_PORT: " + msgContext.get(MessageContext.WSDL_PORT));
+      log.info("WSDL_OPERATION: " + msgContext.get(MessageContext.WSDL_OPERATION));
       return input;
    }
 }
