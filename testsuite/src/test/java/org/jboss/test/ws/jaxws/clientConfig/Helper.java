8a8c54ba37feb2e8a67b6091a4754a06fb589835
alessio.soldano <alessio.soldano@jboss.com>
2012-06-08 13:36:40 +0000

    [JBWS-3335] Synch with latest jbossws-api + modify testcase to show how to only leverage jbossws-api for client configuration setup

--- testsuite/src/test/java/org/jboss/test/ws/jaxws/clientConfig/Helper.java
+++ testsuite/src/test/java/org/jboss/test/ws/jaxws/clientConfig/Helper.java
@@ -22,10 +22,7 @@
 package org.jboss.test.ws.jaxws.clientConfig;
 
 import java.net.URL;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.ServiceLoader;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.BindingProvider;
@@ -33,15 +30,8 @@ import javax.xml.ws.Service;
 import javax.xml.ws.handler.Handler;
 
 import org.jboss.test.helper.ClientHelper;
+import org.jboss.ws.api.configuration.ClientConfigUtil;
 import org.jboss.ws.api.configuration.ClientConfigurer;
-import org.jboss.wsf.spi.SPIProvider;
-import org.jboss.wsf.spi.SPIProviderResolver;
-import org.jboss.wsf.spi.classloading.ClassLoaderProvider;
-import org.jboss.wsf.spi.management.ServerConfig;
-import org.jboss.wsf.spi.management.ServerConfigFactory;
-import org.jboss.wsf.spi.metadata.config.ClientConfig;
-import org.jboss.wsf.spi.metadata.j2ee.serviceref.UnifiedHandlerChainMetaData;
-import org.jboss.wsf.spi.metadata.j2ee.serviceref.UnifiedHandlerMetaData;
 
 /**
  * Verifies client configuration setup
@@ -55,11 +45,7 @@ public class Helper implements ClientHelper
    
    public boolean testClientConfigurer()
    {
-      Iterator<ClientConfigurer> it = ServiceLoader.load(ClientConfigurer.class).iterator();
-      if (!it.hasNext()) {
-         return false;
-      }
-      ClientConfigurer configurer = it.next();
+      ClientConfigurer configurer = ClientConfigUtil.resolveClientConfigurer();
       if (configurer == null) {
          return false;
       }
@@ -80,8 +66,7 @@ public class Helper implements ClientHelper
       hc.add(new UserHandler());
       bp.getBinding().setHandlerChain(hc);
       
-      ClientConfigurer configurer = ServiceLoader.load(ClientConfigurer.class).iterator().next();
-      configurer.addConfigHandlers(bp, "META-INF/jaxws-client-config.xml", "Custom Client Config");
+      ClientConfigUtil.setConfigHandlers(bp, "META-INF/jaxws-client-config.xml", "Custom Client Config");
 
       String resStr = port.echo("Kermit");
       return ("Kermit|RoutOut|CustomOut|UserOut|LogOut|endpoint|LogIn|UserIn|CustomIn|RoutIn".equals(resStr));
@@ -101,15 +86,15 @@ public class Helper implements ClientHelper
       hc.add(new UserHandler());
       bp.getBinding().setHandlerChain(hc);
       
-      ClientConfigurer configurer = ServiceLoader.load(ClientConfigurer.class).iterator().next();
-      configurer.addConfigHandlers(bp, "META-INF/jaxws-client-config.xml", "Custom Client Config");
+      ClientConfigurer configurer = ClientConfigUtil.resolveClientConfigurer();
+      configurer.setConfigHandlers(bp, "META-INF/jaxws-client-config.xml", "Custom Client Config");
 
       String resStr = port.echo("Kermit");
       if (!"Kermit|RoutOut|CustomOut|UserOut|LogOut|endpoint|LogIn|UserIn|CustomIn|RoutIn".equals(resStr)) {
          return false;
       }
       
-      configurer.addConfigHandlers(bp, "META-INF/jaxws-client-config.xml", "Another Client Config");
+      configurer.setConfigHandlers(bp, "META-INF/jaxws-client-config.xml", "Another Client Config");
       
       resStr = port.echo("Kermit");
       return ("Kermit|RoutOut|UserOut|endpoint|UserIn|RoutIn".equals(resStr));
@@ -128,13 +113,10 @@ public class Helper implements ClientHelper
       QName serviceName = new QName("http://clientConfig.jaxws.ws.test.jboss.org/", "EndpointImplService");
       URL wsdlURL = new URL(address + "?wsdl");
       
-      ServerConfig sc = getServerConfig();
-      ClientConfig defaultConfig = getAndVerifyDefaultClientConfiguration(sc);
-      
+      // -- modify default conf --
       try
       {
-         // -- modify default conf --
-         modifyDefaultClientConfiguration(defaultConfig);
+         TestUtils.modifyDefaultClientConfiguration(TestUtils.getAndVerifyDefaultClientConfiguration());
          // --
          
          Service service = Service.create(wsdlURL, serviceName);
@@ -147,14 +129,12 @@ public class Helper implements ClientHelper
          bp.getBinding().setHandlerChain(hc);
          
          String resStr = port.echo("Kermit");
-         System.out.println("--> ** " + resStr);
          return ("Kermit|UserOut|LogOut|endpoint|LogIn|UserIn".equals(resStr));
       }
       finally
       {
          // -- restore default conf --
-         defaultConfig.setPostHandlerChains(null);
-         defaultConfig.setPreHandlerChains(null);
+         TestUtils.cleanupClientConfig();
          // --
       }
    }
@@ -171,12 +151,11 @@ public class Helper implements ClientHelper
       QName serviceName = new QName("http://clientConfig.jaxws.ws.test.jboss.org/", "EndpointImplService");
       URL wsdlURL = new URL(address + "?wsdl");
       
-      ServerConfig sc = getServerConfig();
       final String testConfigName = "MyTestConfig";
       try
       {
          //-- add test client configuration
-         addTestCaseClientConfiguration(sc, testConfigName);
+         TestUtils.addTestCaseClientConfiguration(testConfigName);
          // --
          
          Service service = Service.create(wsdlURL, serviceName);
@@ -188,8 +167,7 @@ public class Helper implements ClientHelper
          hc.add(new UserHandler());
          bp.getBinding().setHandlerChain(hc);
          
-         ClientConfigurer configurer = ServiceLoader.load(ClientConfigurer.class).iterator().next();
-         configurer.addConfigHandlers(bp, null, testConfigName);
+         ClientConfigUtil.setConfigHandlers(bp, null, testConfigName);
          
          String resStr = port.echo("Kermit");
          return ("Kermit|RoutOut|UserOut|endpoint|UserIn|RoutIn".equals(resStr));
@@ -197,72 +175,11 @@ public class Helper implements ClientHelper
       finally
       {
          // -- remove test client configuration --
-         removeTestCaseClientConfiguration(sc, testConfigName);
+         TestUtils.removeTestCaseClientConfiguration(testConfigName);
          // --
       }
    }
    
-   private ClientConfig getAndVerifyDefaultClientConfiguration(ServerConfig sc) throws Exception {
-      ClientConfig defaultConfig = null;
-      for (ClientConfig c : sc.getClientConfigs()) {
-         if (ClientConfig.STANDARD_CLIENT_CONFIG.equals(c.getConfigName())) {
-            defaultConfig = c;
-         }
-      }
-      if (defaultConfig == null) {
-         throw new Exception("Missing AS client config '" + ClientConfig.STANDARD_CLIENT_CONFIG + "'!");
-      }
-      List<UnifiedHandlerChainMetaData> preHC = defaultConfig.getPreHandlerChains();
-      List<UnifiedHandlerChainMetaData> postHC = defaultConfig.getPostHandlerChains();
-      if ((preHC != null && !preHC.isEmpty()) || (postHC != null && !postHC.isEmpty())) {
-         throw new Exception("'" + ClientConfig.STANDARD_CLIENT_CONFIG + "' is not empty!");
-      }
-      return defaultConfig;
-   }
-   
-   private void modifyDefaultClientConfiguration(ClientConfig defaultConfig) {
-      UnifiedHandlerChainMetaData uhcmd = new UnifiedHandlerChainMetaData();
-      UnifiedHandlerMetaData handler = new UnifiedHandlerMetaData();
-      handler.setHandlerClass("org.jboss.test.ws.jaxws.clientConfig.LogHandler");
-      handler.setHandlerName("Log Handler");
-      uhcmd.addHandler(handler);
-      List<UnifiedHandlerChainMetaData> postHC = new LinkedList<UnifiedHandlerChainMetaData>();
-      postHC.add(uhcmd);
-      defaultConfig.setPostHandlerChains(postHC);
-   }
-   
-   private void addTestCaseClientConfiguration(ServerConfig sc, String testConfigName) {
-      UnifiedHandlerChainMetaData uhcmd = new UnifiedHandlerChainMetaData();
-      UnifiedHandlerMetaData handler = new UnifiedHandlerMetaData();
-      handler.setHandlerClass("org.jboss.test.ws.jaxws.clientConfig.RoutingHandler");
-      handler.setHandlerName("Routing Handler");
-      uhcmd.addHandler(handler);
-      ClientConfig config = new ClientConfig();
-      config.setConfigName(testConfigName);
-      List<UnifiedHandlerChainMetaData> preHC = new LinkedList<UnifiedHandlerChainMetaData>();
-      preHC.add(uhcmd);
-      config.setPreHandlerChains(preHC);
-      sc.addClientConfig(config);
-   }
-   
-   private void removeTestCaseClientConfiguration(ServerConfig sc, String testConfigName) {
-      Iterator<ClientConfig> it = sc.getClientConfigs().iterator();
-      while (it.hasNext()) {
-         ClientConfig c = it.next();
-         if (testConfigName.equals(c.getConfigName())) {
-            it.remove();
-            break;
-         }
-      }
-   }
-   
-   private ServerConfig getServerConfig()
-   {
-      final ClassLoader cl = ClassLoaderProvider.getDefaultProvider().getServerIntegrationClassLoader();
-      SPIProvider spiProvider = SPIProviderResolver.getInstance(cl).getProvider();
-      return spiProvider.getSPI(ServerConfigFactory.class, cl).getServerConfig();
-   }
-
    @Override
    public void setTargetEndpoint(String address)
    {
