e93d78c739a656a41ce7df43544c03c750ff77e1
alessio.soldano@jboss.com <alessio.soldano@jboss.com@50344849-fb0f-0410-a53f-a94597aac05c>
2012-06-08 09:48:28 +0000

    Fixing missing undeploy of test client jar

--- shared-testsuite/trunk/testsuite/src/test/java/org/jboss/test/ws/jaxws/jbws3140/JBWS3140TestCase.java
+++ shared-testsuite/trunk/testsuite/src/test/java/org/jboss/test/ws/jaxws/jbws3140/JBWS3140TestCase.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -26,11 +26,9 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import junit.framework.Test;
 
 import org.jboss.wsf.test.JBossWSTest;
 import org.jboss.wsf.test.JBossWSTestHelper;
-import org.jboss.wsf.test.JBossWSTestSetup;
 
 public class JBWS3140TestCase extends JBossWSTest
 {
@@ -48,6 +46,7 @@ public class JBWS3140TestCase extends JBossWSTest
          assertTrue("Expected message wasn't found in response: " + result, result.indexOf(expectedDetail) > -1);
       } finally {
          JBossWSTestHelper.undeploy("jbws3140-responses-server.war");
+         JBossWSTestHelper.undeploy("jbws3140-client.war");
       }
    }
 
@@ -62,6 +61,7 @@ public class JBWS3140TestCase extends JBossWSTest
          assertTrue("Expected string wasn't found in response: " + result, result.indexOf(expected) > -1);
       } finally {
          JBossWSTestHelper.undeploy("jbws3140-server.war");
+         JBossWSTestHelper.undeploy("jbws3140-client.war");
       }
    }
 
