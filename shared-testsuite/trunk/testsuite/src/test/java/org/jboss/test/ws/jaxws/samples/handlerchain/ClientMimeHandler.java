c172ca496f62d78658cb44b5cf9903e59be5a925
alessio.soldano@jboss.com <alessio.soldano@jboss.com@50344849-fb0f-0410-a53f-a94597aac05c>
2012-09-17 11:15:30 +0000

    [JBWS-3547] Fix cookies handling in HandlerChainTestCase

--- shared-testsuite/trunk/testsuite/src/test/java/org/jboss/test/ws/jaxws/samples/handlerchain/ClientMimeHandler.java
+++ shared-testsuite/trunk/testsuite/src/test/java/org/jboss/test/ws/jaxws/samples/handlerchain/ClientMimeHandler.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -21,7 +21,10 @@
  */
 package org.jboss.test.ws.jaxws.samples.handlerchain;
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.xml.soap.MimeHeaders;
 import javax.xml.soap.SOAPMessage;
@@ -35,6 +38,7 @@ import org.jboss.ws.api.handler.GenericSOAPHandler;
  * A client side handler
  *
  * @author Thomas.Diesler@jboss.org
+ * @author alessio.soldano@jboss.com
  * @since 08-Oct-2005
  */
 public class ClientMimeHandler extends GenericSOAPHandler
@@ -48,9 +52,15 @@ public class ClientMimeHandler extends GenericSOAPHandler
    {
       log.info("handleOutbound");
 
+      // legacy JBossWS-Native approach
       SOAPMessage soapMessage = ((SOAPMessageContext)msgContext).getMessage();
       MimeHeaders mimeHeaders = soapMessage.getMimeHeaders();
       mimeHeaders.setHeader("Cookie", "client-cookie=true");
+      
+      // proper approach through MessageContext.HTTP_REQUEST_HEADERS
+      Map<String, List<String>> httpHeaders = new HashMap<String, List<String>>();
+      httpHeaders.put("Cookie", Collections.singletonList("client-cookie=true"));
+      msgContext.put(MessageContext.HTTP_REQUEST_HEADERS, httpHeaders);
 
       inboundCookie = null;
       
@@ -61,9 +71,21 @@ public class ClientMimeHandler extends GenericSOAPHandler
    {
       log.info("handleInbound");
 
+      //legacy JBossWS-Native approach
       SOAPMessage soapMessage = ((SOAPMessageContext)msgContext).getMessage();
       MimeHeaders mimeHeaders = soapMessage.getMimeHeaders();
       String[] cookies = mimeHeaders.getHeader("Set-Cookie");
+      
+      // proper approach through MessageContext.HTTP_RESPONSE_HEADERS
+      if (cookies == null) {
+         @SuppressWarnings("unchecked")
+         Map<String, List<String>> httpHeaders = (Map<String, List<String>>) msgContext.get(MessageContext.HTTP_RESPONSE_HEADERS);
+         List<String> l = httpHeaders.get("Set-Cookie");
+         if (l != null && !l.isEmpty()) {
+            cookies = l.toArray(new String[l.size()]);
+         }
+      }
+      
       if (cookies != null && cookies.length == 1)
          inboundCookie = cookies[0];
 
