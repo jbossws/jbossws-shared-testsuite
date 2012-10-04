ab4b2b5d1d98ef0c6a4066cea6a94318bf3af91f
richard.opalka@jboss.com <richard.opalka@jboss.com@50344849-fb0f-0410-a53f-a94597aac05c>
2012-10-04 10:40:34 +0000

    [JBWS-3552] providing test case

---
+++ shared-testsuite/trunk/testsuite/src/test/java/org/jboss/test/ws/jaxws/jbws3552/EndpointImpl.java
@@ -0,0 +1,59 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.ws.jaxws.jbws3552;
+
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "org.jboss.test.ws.jaxws.jbws3552.EndpointIface")
+public class EndpointImpl {
+    public AdaptedObjectCA echoCA(AdaptedObjectCA ao) {
+        return ao;
+    }
+
+    public AdaptedObjectFA echoFA(AdaptedObjectFA ao) {
+        return ao;
+    }
+
+    public AdaptedObjectGA echoGA(AdaptedObjectGA ao) {
+        return ao;
+    }
+
+    public AdaptedObjectMA echoMA(AdaptedObjectMA ao) {
+        return ao;
+    }
+
+    public void throwExceptionCA() throws AdaptedExceptionCA {
+        throw new AdaptedExceptionCA("exception message", "exception description", 666, new ComplexObjectCA("c", "d"));
+    }
+
+    public void throwExceptionFA() throws AdaptedExceptionFA {
+        throw new AdaptedExceptionFA("exception message", "exception description", 666, new ComplexObjectFA("c", "d"));
+    }
+
+    public void throwExceptionGA() throws AdaptedExceptionGA {
+        throw new AdaptedExceptionGA("exception message", "exception description", 666, new ComplexObjectGA("c", "d"));
+    }
+
+    public void throwExceptionMA() throws AdaptedExceptionMA {
+        throw new AdaptedExceptionMA("exception message", "exception description", 666, new ComplexObjectMA("c", "d"));
+    }
+}
