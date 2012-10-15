e9d57d8441e350e65e4d8f5af59f8f9015715f07
alessio.soldano@jboss.com <alessio.soldano@jboss.com@50344849-fb0f-0410-a53f-a94597aac05c>
2012-10-15 13:54:47 +0000

    [JBWS-3558] Tagging jbossws-shared-testsuite-4.1.0.Final

---
+++ shared-testsuite/tags/jbossws-shared-testsuite-4.1.0.Final/testsuite/src/test/java/org/jboss/test/ws/jaxws/jbws3552/AdaptedObjectCA.java
@@ -0,0 +1,64 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.ws.jaxws.jbws3552;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class AdaptedObjectCA {
+    private String message;
+    private String description;
+    private int code;
+    private ComplexObjectCA complexObject;
+
+    public AdaptedObjectCA() {
+    }
+
+    public AdaptedObjectCA(String message, String description, int code, ComplexObjectCA complexObject) {
+        this.message = message;
+        this.description = description;
+        this.code = code;
+        this.complexObject = complexObject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public ComplexObjectCA getComplexObject() {
+        return complexObject;
+    }
+
+    public String toString() {
+        return message + "," + description + "," + code + "," + complexObject;
+    }
+}
